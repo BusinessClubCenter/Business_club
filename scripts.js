d223323cd51ffcf07f0110411e768e6c0a14cef2
Business_club <azzonov2526@gmail.com>
2025-02-10 23:26:27 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,10 @@
-// Скрипт для анимации текста при загрузке
+// Анимация текста
 window.addEventListener('DOMContentLoaded', () => {
-    const text = document.querySelector('.text');
-    text.classList.add('fade-in');
+    document.querySelector('.text')?.classList.add('fade-in');
 });
 
-// Смена цвета кнопки при наведении
-const navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
-navbarLinks.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        link.style.color = '#48D1CC';
-    });
-    link.addEventListener('mouseout', () => {
-        link.style.color = 'white';
-    });
+// Изменение цвета ссылок при наведении
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('mouseover', () => link.style.color = '#48D1CC');
+    link.addEventListener('mouseout', () => link.style.color = 'white');
 });
